d5ae114faf74229ed9dfc665e048c8c01564b4a2
Rostyslaw Z <rostykzadyraichuk010@gmail.com>
2020-02-07 01:18:50 +0200

    add polindrome check program

---
+++ javaCore10/lesson/task1/App.java
@@ -0,0 +1,26 @@
+package mainPackage.javaCore10.lesson.task1;
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter word: ");
+        try {
+            String word = sc.nextLine();
+            if (word.length() != 5){
+                throw new WordLengthException("Word length not correct! Enter word from 5 chars");
+            }
+            if (word.toLowerCase().charAt(0) == word.toLowerCase().charAt(4)
+                && word.toLowerCase().charAt(1) == word.toLowerCase().charAt(3)) {
+                System.out.println("This word is polindrome");
+            } else {
+                System.out.println("This word isn't polindrome");
+            }
+        } catch (WordLengthException e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+}
---
+++ javaCore10/lesson/task1/WordLengthException.java
@@ -0,0 +1,7 @@
+package mainPackage.javaCore10.lesson.task1;
+
+public class WordLengthException extends Exception {
+    public WordLengthException(String message) {
+        super(message);
+    }
+}
