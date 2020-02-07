067173685b02ae21b0bac50f8f5073505e6c8064
Rostyslaw Z <rostykzadyraichuk010@gmail.com>
2020-02-07 02:20:35 +0200

    add program which return quantity of sentences

---
+++ javaCore10/lesson/task4/App.java
@@ -0,0 +1,18 @@
+package mainPackage.javaCore10.lesson.task4;
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int count = 0;
+        StringBuilder text = new StringBuilder(sc.nextLine());
+        for (int i = 0; i < text.length(); i++){
+            if (text.charAt(i) == '.' || text.charAt(i) == '!'
+                || text.charAt(i) == '?') {
+                count++;
+            }
+        }
+        System.out.println("Text has " + count + " sentences");
+    }
+}
