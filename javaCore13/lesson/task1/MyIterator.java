76963329e3869536a6f7dbfdc17461a92dcf45d6
Rostyslaw Z <rostykzadyraichuk010@gmail.com>
2020-02-15 18:01:03 +0200

    JavaCore13 hometask

---
+++ javaCore13/lesson/task1/MyIterator.java
@@ -0,0 +1,10 @@
+package mainPackage.javaCore13.lesson.task1;
+
+import java.util.Iterator;
+import java.util.List;
+
+public class MyIterator {
+    public static Iterator createIterator(List list) {
+        return list.iterator();
+    }
+}
