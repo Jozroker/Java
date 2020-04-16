1a6da62ccd6804bb1a5b0387a5935458164a8a4a
Rostyslaw <rostykzadyraichuk010@gmail.com>
2020-04-16 20:29:23 +0300

    reformat code

--- javaCore14/lesson/task1/AgeComparator.java
+++ javaCore14/lesson/task1/AgeComparator.java
@@ -6,9 +6,9 @@ public class AgeComparator implements Comparator<Users> {
 
     @Override
     public int compare(Users u1, Users u2) {
-        if(u1.getAge() == u2.getAge()) {
+        if (u1.getAge() == u2.getAge()) {
             return 0;
-        } else if(u1.getAge() > u2.getAge()) {
+        } else if (u1.getAge() > u2.getAge()) {
             return 1;
         } else {
             return -1;
